d5fbff4e0ee5b755853a2a65a85811ac880a5c4d
Muhammed  Alsin <81310513+muhammedalsin@users.noreply.github.com>
2025-10-14 16:36:19 +0300

    first commit

---
+++ models/Todo.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  paragraph: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const Todo = require("./models/Todo");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Get all todos
+app.get("/", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+// Add new todo
+app.post("/add", async (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text,
+    paragraph: req.body.paragraph,
+    completed: req.body.completed,
+  });
+  await newTodo.save();
+  res.json(newTodo);
+});
+
+// Toggle todo completed
+app.put("/toggle/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.completed = !todo.completed;
+  await todo.save();
+  res.json(todo);
+});
+
+// Delete todo
+app.delete("/delete/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
